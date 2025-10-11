5affe472268e9098ec32be196d0a015e809597b9
EdaKocaman <eda.kkocaman@gmail.com>
2025-10-11 16:14:56 +0300

    init: spring boot project setup with Maven

---
+++ real-estate-management/src/test/java/com/eda/real_estate_management/RealEstateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eda.real_estate_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
